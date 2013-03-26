0b1f35b002bb7cda8da676d9880fdbf92d155975
Tom Caputi <emailme3546@gmail.com>
2013-03-26 12:28:18 -0400

    Fixed get bug

--- src/net/epsilonlabs/datamanagementefficient/library/Cache.java
+++ src/net/epsilonlabs/datamanagementefficient/library/Cache.java
@@ -4,6 +4,7 @@ import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.util.Log;
 import android.util.SparseArray;
 
 /**
@@ -13,16 +14,16 @@ import android.util.SparseArray;
  *
  */
 public class Cache {
-	
+
 	private Map<Class<?>, SparseArray<SoftReference<Object>>> cache; //The actual map that stores all the cached Objects
-	
+
 	/**
 	 * Instantiates the cache map
 	 */
 	public Cache(){
 		this.cache = new HashMap<Class<?>, SparseArray<SoftReference<Object>>>();
 	}
-	
+
 	/**
 	 * Gets an object from the map based on its Class and id number. Returns null if there is no such object
 	 * @param cls the class of the object
@@ -31,11 +32,13 @@ public class Cache {
 	 */
 	@SuppressWarnings("unchecked")
 	public <T> T get(Class<T> cls, int id){
-		SparseArray<SoftReference<Object>> classCache = cache.get(cls);
-		if(classCache == null) return null;
-		return (T) classCache.get(id).get();
+			SparseArray<SoftReference<Object>> classCache = cache.get(cls);
+			if(classCache == null) return null;
+			if(classCache.get(id) == null) return null;
+			return (T) classCache.get(id).get();
+		
 	}
-	
+
 	/**
 	 * Places an object into the cache. Replaces existing objects with the same class and id number.
 	 * @param obj the object to be placed into the cache
@@ -48,7 +51,7 @@ public class Cache {
 		classCache.put(id, new SoftReference<Object>(obj));
 		cache.put(cls, classCache);
 	}
-	
+
 	/**
 	 * Removes an object from the cache.
 	 * @param cls the class of the object
